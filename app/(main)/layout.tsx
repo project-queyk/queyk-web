45136bb97e7fd1bd316460a0067d8237824bbe6d
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-03-26 03:11:44 +0800

    feat: Redirect the unauthenticated user to the signin page when they visit the main pages

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,8 +1,20 @@
+import { redirect } from "next/navigation";
+
+import { auth } from "@/auth";
+
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import Header from "@/components/Header";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth();
+
+  if (!session) return redirect("/signin");
+
   return (
     <SidebarProvider>
       <AppSidebar />
