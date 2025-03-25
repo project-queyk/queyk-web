3b8db40e02196819ae21747bcf00eaadbdc5b63d
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-03-26 03:12:43 +0800

    refactor: Add server actions for both the sign in and sign out

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,18 +1,11 @@
-import { signIn } from "@/auth";
+import { signInAction } from "@/lib/auth-actions";
 
 import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
     <div className="my-24 flex items-center justify-center">
-      <form
-        action={async () => {
-          "use server";
-          await signIn("google");
-        }}
-      >
-        <Button type="submit">Sign in</Button>
-      </form>
+      <Button onClick={signInAction}>Sign in</Button>
     </div>
   );
 }
