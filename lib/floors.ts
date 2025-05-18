3386b70ca4234897db9d27c57212e51bc6eead5a
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-05-18 13:24:39 +0800

    feat: Add floor plans with images for all building levels

---
+++ lib/floors.ts
@@ -0,0 +1,27 @@
+export const floors = [
+  {
+    id: "ground",
+    title: "Ground Floor",
+    imageSrc: "/floor_plans/ground-floor.jpg",
+  },
+  {
+    id: "first",
+    title: "1st Floor",
+    imageSrc: "/floor_plans/first-floor.jpg",
+  },
+  {
+    id: "second",
+    title: "2nd Floor",
+    imageSrc: "/floor_plans/second-floor.jpg",
+  },
+  {
+    id: "third",
+    title: "3rd Floor",
+    imageSrc: "/floor_plans/third-floor.jpg",
+  },
+  {
+    id: "fourth",
+    title: "4th Floor",
+    imageSrc: "/floor_plans/fourth-floor.jpg",
+  },
+];
