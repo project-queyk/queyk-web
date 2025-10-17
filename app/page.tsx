9f7665be89ae41bebed2a83ec660b07815c60c53
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-10-18 06:48:19 +0800

    fix: Remove redirect logic from Home component

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { auth } from "@/auth";
 import { Metadata } from "next";
 
 import LandingPage from "@/components/LandingPage";
-import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Queyk - Open-source earthquake early warning system",
@@ -10,13 +9,6 @@ export const metadata: Metadata = {
 
 export default async function Home() {
   const session = await auth();
-
-  if (session) {
-    if (session.user.role === "admin") {
-      return redirect("/dashboard");
-    }
-    return redirect("/evacuation-plan");
-  }
 
   return <LandingPage session={session} />;
 }
