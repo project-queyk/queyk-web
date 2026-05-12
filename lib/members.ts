c6e5a245440969cfc39fe9c601ca7671cd502bd5
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2026-05-13 05:20:15 +0800

    chore: Update team images to use PNG format and reorganize originals

--- lib/members.ts
+++ lib/members.ts
@@ -1,8 +1,8 @@
-import madriaga from "@/public/team/madriaga.jpeg";
-import cabantac from "@/public/team/cabantac.jpg";
+import madriaga from "@/public/team/madriaga.png";
+import cabantac from "@/public/team/cabantac.png";
 import aspacio from "@/public/team/aspacio.png";
-import arugay from "@/public/team/arugay.jpg";
-import carbo from "@/public/team/carbo.jpeg";
+import arugay from "@/public/team/arugay.png";
+import carbo from "@/public/team/carbo.png";
 
 export const members = [
   {
